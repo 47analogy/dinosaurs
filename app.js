15af7972cbaf609516c4a9c754dd057d32cfa477
analogy47 <actweed47@gmail.com>
2020-11-27 02:35:54 -0500

    feat: Create dino and human objects
    Human object is being created in an IIFE when the form is submitted.

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
 // Create Dino Constructor
+class Dino {
+  constructor(name, image) {
+    this.name = name;
+    this.image = image;
+  }
+}
 
 // Create Dino Objects
+const anklyosaurus = new Dino('anklyosaurus', './images/anklyosaurus.png');
+const brachiosaurus = new Dino('brachiosaurus', './images/brachiosaurus.png');
+const telasmosaurus = new Dino('telasmosaurus', './images/telasmosaurus.png');
+const pigeon = new Dino('pigeon', './images/pigeon.png');
+const pteranodon = new Dino('pteranodon', './images/pteranodon.png');
+const stegosaurus = new Dino('stegosaurus', './images/stegosaurus.png');
+const tracks = new Dino('tracks', './images/tracks.png');
+const triceratops = new Dino('triceratops', './images/triceratops.png');
+const tyrannosaurusRex = new Dino('anklyosaurus', './images/anklyosaurus.png');
 
 // Create Human Object
+class Human extends Dino {
+  constructor(name, image, heightFoot, heightInch, weight, diet) {
+    super(name, image);
+    this.heightFoot = heightFoot;
+    this.heightInch = heightInch;
+    this.weight = weight;
+    this.diet = diet;
+  }
+}
 
 // Use IIFE to get human data from form
+(function () {
+  const form = document.querySelector('#dino-compare');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.querySelector('#name').value;
+    const image = './images/human.png';
+    const heightFoot = document.querySelector('#feet').value;
+    const heightInch = document.querySelector('#inches').value;
+    const weight = document.querySelector('#weight').value;
+    const diet = document.querySelector('#diet').value;
+
+    // store the data
+    const human = new Human(name, image, heightFoot, heightInch, weight, diet);
+    console.log('form submit', human);
+  });
+})();
+
+// TODO: VALIDATION
+function formValidate() {}
 
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches.
