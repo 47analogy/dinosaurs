ec58e37cf20bf0777eda3b164b7738957b234526
analogy47 <actweed47@gmail.com>
2020-11-30 22:26:09 -0500

    display tiles in random order

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const { Dinos } = require('./dino.json');
 
-class Tile {
+/** Create TileCharacter Constructor
+ * @description Represents a TileCharacter
+ * @constructor
+ * @param {string} name - The name of the TileCharacter
+ * @param {string} image - The relative path to the image of the TileCharacter
+ * @param {string} weight - The weight of the TileCharacter
+ * @param {string} diet - The diet of the TileCharacter
+ */
+class TileCharacter {
   constructor(name, image, weight, diet) {
     this.name = name;
     this.image = image;
@@ -14,8 +22,14 @@ class Tile {
  * @constructor
  * @param {string} name - The name of the Dino
  * @param {string} image - The relative path to the image of the Dino
+ * @param {string} weight - The weight of the Dino
+ * @param {string} height - The height of the Dino
+ * @param {string} diet - The diet of the Dino
+ * @param {string} where - The location of the Dino
+ * @param {string} when - The period of the Dino
+ * @param {string} fact - The fun fact of the Dino
  */
-class Dino extends Tile {
+class Dino extends TileCharacter {
   constructor(name, image, weight, height, diet, where, when, fact) {
     super(name, image, weight, diet);
     this.height = height;
@@ -25,9 +39,25 @@ class Dino extends Tile {
   }
 }
 
-let listOfDinoObjs = [];
+/** Create Human Class
+ * @description Represents a Human
+ * @constructor
+ * @param {string} name - The name of the Human
+ * @param {string} image - The relative path to the image of the human
+ * @param {number} heightFoot - The height foot portion of the human
+ * @param {number} heightInch - The height inches portion of the human
+ * @param {number} weight - The weight of the human
+ * @param {string} diet - The diet of the human
+ */
+class Human extends TileCharacter {
+  constructor(name, image, weight, diet, heightFoot, heightInch) {
+    super(name, image, weight, diet);
+    this.heightFoot = heightFoot;
+    this.heightInch = heightInch;
+  }
+}
 
-// // Use IIFE to create Dino Objects
+// Use IIFE to create Dino Objects
 (function () {
   listOfDinoObjs = Dinos.map((dino) => {
     const instance = `${dino.species
@@ -46,26 +76,6 @@ let listOfDinoObjs = [];
     ));
   });
 })(this);
-
-console.log('list', listOfDinoObjs);
-
-/** Create Human Class
- * @description Represents a Human
- * @constructor
- * @param {string} name - The name of the Human
- * @param {string} image - The relative path to the image of the human
- * @param {number} heightFoot - The height foot portion of the human
- * @param {number} heightInch - The height inches portion of the human
- * @param {number} weight - The weight of the human
- * @param {string} diet - The diet of the human
- */
-class Human extends Tile {
-  constructor(name, image, weight, diet, heightFoot, heightInch) {
-    super(name, image, weight, diet);
-    this.heightFoot = heightFoot;
-    this.heightInch = heightInch;
-  }
-}
 
 // Use IIFE to get human data from form
 (function () {
@@ -90,15 +100,13 @@ class Human extends Tile {
 // TODO: VALIDATION
 function formValidate() {}
 
-/** Create compare method
- * @description Represents a method to compare dino and human facts
- * @param {array} dinos - Array of objects for dinos
- * @param {obj} human - Object containing human facts
- * @return {obj} speciesName, weight
- */
-
 // Create Dino Compare Method 1
-// NOTE: Weight in JSON file is in lbs, height in inches.
+/**compareWeight()
+ * @description Represents a Dino class method to compare dino and human weights
+ * Both weights are in lbs
+ * @param {humanWeight} human - weight of human
+ * @return {string} - string comparison fact (Dino object property)
+ */
 Dino.prototype.compareWeight = function (humanWeight) {
   if (this.weight > humanWeight) {
     this.weightCompare = `${this.name}s are ${
@@ -114,8 +122,16 @@ Dino.prototype.compareWeight = function (humanWeight) {
 };
 
 // Create Dino Compare Method 2
-// NOTE: Weight in JSON file is in lbs, height in inches.
-Dino.prototype.compareHeight = function (humanHeight) {
+/**compareHeight()
+ * @description Represents a Dino class method to compare dino and human heights
+ * Dino heights are in inches, human height is ft and inches
+ * @param {humanHeightFt} human - height of human feet portion
+ * @param {humanHeightInch} human - height of human feet portion
+ * @return {string} - string comparison fact (Dino object property)
+ */
+Dino.prototype.compareHeight = function (humanHeightFt, humanHeightInch) {
+  const humanHeight = humanHeightFt * 12 + humanHeightInch; // convert to inches
+
   if (this.height > humanHeight) {
     this.heightCompare = `${this.name}s are ${
       this.height - humanHeight
@@ -130,17 +146,102 @@ Dino.prototype.compareHeight = function (humanHeight) {
 };
 
 // Create Dino Compare Method 3
-// NOTE: Weight in JSON file is in lbs, height in inches.
+/**compareDiet()
+ * @description Represents a Dino class method to compare dino and human diets
+ * @param {humanDiet} human - diet of human
+ * @return {string} - string comparison fact (Dino object property)
+ */
 Dino.prototype.compareDiet = function (humanDiet) {
   this.dietCompare = `${this.name}s are ${this.diet}s, humans are ${humanDiet}`;
 };
 
-// create an array of objects for each dino, human
+// create an random array of objects for each dino, human
+function createTileData(dinoArr, humanObject) {
+  // loop thru each dino and add 3 compare methods
+  dinoArr.map((dino) => {
+    let { name } = dino;
+    name = `${name.charAt(0).toLowerCase()}${name.slice(1)}`.replace(/\s/g, '');
+    this[name].compareWeight(humanObject.weight);
+    this[name].compareHeight(humanObject.heightFoot, humanObject.heightInch);
+    this[name].compareDiet(humanObject.diet);
+  });
+  // add everything to array
+  const tilesArr = [...dinoArr, humanObject];
+  console.log('tiles', tilesArr);
+  //create random order - look into Fisher-Yates shuffle
+  const randomTilesArr = tilesArr.sort(() => Math.random() - 0.5);
+  generateTiles(randomTilesArr);
+}
 
 // Generate Tiles for each Dino in Array
+function generateTiles(arr) {
+  arr.forEach((tile, index) => {
+    const tileElement = document.createElement('div');
+    const tileTitle = document.createElement('h3');
+    const tileImage = document.createElement('img');
+    const tileFact = document.createElement('p');
 
-// Add tiles to DOM
+    tileElement.classList.add('grid-item');
+
+    grid.appendChild(tileElement);
+
+    tileElement.id = index; // temp
+    tileTitle.innerHTML = tile.name || '';
+    tileImage.src = 'https://placeimg.com/140/180/any';
+    // tileImage.src = tile.image; // TODO: FIX ISSUE (not showing)
+    tileImage.alt = `${tile.name} Image`;
+    tileFact.innerHTML = tile.fact || ''; // TODO: Generate random facts
+    tileElement.appendChild(tileTitle);
+    tileElement.appendChild(tileImage);
+    tileElement.appendChild(tileFact);
+  });
+  // drawTiles(board);
+}
 
 // Remove form from screen
 
 // On button click, prepare and display infographic
+
+// // Add tiles to DOM
+// function drawTiles(board) {
+//   const grid = document.querySelector('#grid');
+//   grid.appendChild(board);
+// }
+
+// dino_img = `../images/${dinoObject.image}`
+// grid.innerHTML = `
+//      <div class="grid">
+//          <h3></h3>
+//          <img src="${image_path}"
+//          <p></p>
+//      </div>
+// `
+
+// // Create Dino Compare Method 3
+// // NOTE: Weight in JSON file is in lbs, height in inches.
+// Dino.prototype.diet = function (humanDiet) {
+//     // filter species, weight
+//     // return array
+//     // `xxx are xxx lbs heavier than humans'
+//     // `xxx are xxx lbs lighter than humans than humans'
+//     //};
+
+//     const dinoDiet = [];
+//     const dinoDietFacts = [];
+
+//     for (let dino of Dinos) {
+//       dinoDiet.push({ speciesName: dino.species, height: dino.height });
+//     }
+
+//     // console.log(dinoDiet);
+//     for (let dino of dinoDiets) {
+//       const dinoDiet = dino.diet;
+//       const species = dino.speciesName;
+
+//       //console.log(`${species} are xxx lbs lighter than humans`);
+//       dinoDietFacts.push(`${species} are ${dinoDiet}, humans are ${humanDiet}`);
+
+//       // console.log(dinoDietFacts);
+//       return dinoDietFacts;
+//     }
+//   };
